8b3778fc190d1f84bcc8edf3d1c1e280f2add82a
shivani brijmohan <sbrij001@yahoo.com>
2018-11-26 16:59:31 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,14 @@
 #takes in an argument and returns each arg squared
 #to sqaure something us **
 
-#def square_array(array)
-#  squared = []
-#  array.each do |ele|
-#  squared << ele ** 2
-#  end
-#  return squared
-#end
-
 def square_array(array)
+  squared = []
+  array.each do |ele|
+  squared << ele ** 2
+  end
+  return squared
+end
+
+#def square_array(array)
   array.collect {|ele| ele ** 2}
 end
